347cd87672e72c13c9a09c68bdb922f56d8fc387
xarzu <wm.m.thompson@gmail.com>
2026-05-23 03:09:50 -0500

    Fix: sessionStorage refresh to fix hydration double header

--- app/interaction/page.tsx
+++ app/interaction/page.tsx
@@ -1,44 +1,55 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 
 function InteractionFrame() {
   const searchParams = useSearchParams();
   const redirect = searchParams.get("redirect");
-
-  useEffect(() => {
-    if (!sessionStorage.getItem("interaction_refreshed")) {
-      sessionStorage.setItem("interaction_refreshed", "true");
-      window.location.reload();
-    }
-  }, []);
 
   const iframeSrc = redirect
     ? decodeURIComponent(redirect)
     : "https://bbpress.excellencematter.net/interaction/";
 
   return (
-    <div style={{ 
-      position: 'fixed',
-      top: '64px',
-      left: 0,
-      right: 0,
-      bottom: 0,
-      overflow: 'hidden',
-      zIndex: 0
-    }}>
-      <iframe
-        src={iframeSrc}
-        style={{ 
-          width: '100%', 
-          height: '100%', 
-          border: 'none',
-          display: 'block'
+    <>
+      <noscript>
+        <meta httpEquiv="refresh" content="0" />
+      </noscript>
+      <div
+        id="interaction-frame"
+        style={{
+          position: "fixed",
+          top: "64px",
+          left: 0,
+          right: 0,
+          bottom: 0,
+          overflow: "hidden",
+          zIndex: 0,
         }}
-        title="LikableLogic Forum"
+      >
+        <iframe
+          src={iframeSrc}
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "none",
+            display: "block",
+          }}
+          title="LikableLogic Forum"
+        />
+      </div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            if (!sessionStorage.getItem('interaction_refreshed')) {
+              sessionStorage.setItem('interaction_refreshed', 'true');
+              window.location.reload();
+            }
+          `,
+        }}
       />
-    </div>
+    </>
   );
 }
 
